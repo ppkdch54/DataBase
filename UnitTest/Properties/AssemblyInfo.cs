8a5eb1781003dc216c3ad7cdc2f42ed33100f2b4
张俊 <@DESKTOP-ZJ>
2018-07-27 17:30:56 +0800

    增加单元测试

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4da86c15-f563-494b-99d6-3b1dcf1ae033")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DataBase;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static MySQLHelper mySQLHelper;
+        [ClassInitialize]
+        public static void Init(TestContext testContext)
+        {
+            string server = "127.0.0.1";
+            string port = "3306";
+            string user = "root";
+            string password = "123456";
+            mySQLHelper = new MySQLHelper(server, port, user, password);
+        }
+
+        [TestMethod,Priority(0)]
+        public void ConnectTest()
+        {
+            //连接到数据库
+            mySQLHelper.Connect();
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void InsertData()
+        {
+            //构造需要存储的数据
+            RealTimeData myClass = new RealTimeData() { CreatedTime = DateTime.Now };
+            for (int i = 0; i < 100; i++)
+            {
+                myClass.CreatedTime = myClass.CreatedTime.AddSeconds(10);
+                myClass.OriginValue = i;
+                mySQLHelper.InsertData(myClass);
+            }
+        }
+
+        [TestMethod,Priority(100)]
+        public void DisConnectTest()
+        {
+            //断开数据库连接
+            mySQLHelper.Disconnect();
+        }
+    }
+}
