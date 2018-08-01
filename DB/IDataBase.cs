198034b5c29d981ceeb88398cd0187f3089b4ac9
张俊 <@DESKTOP-ZJ>
2018-08-01 14:25:23 +0800

    更换平台为.net framewrok

---
+++ DB/IDataBase.cs
@@ -0,0 +1,84 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+/// <summary>
+/// 参数表
+/// 实时数据表
+/// </summary>
+namespace DataBase
+{
+    interface IDataBase
+    {
+        /// <summary>
+        /// 连接数据库
+        /// </summary>
+        /// <returns>是否成功</returns>
+        bool Connect();
+
+        /// <summary>
+        /// 断开连接
+        /// </summary>
+        void Disconnect();
+
+        /// <summary>
+        /// 查询参数
+        /// </summary>
+        /// <param name="name">参数名称</param>
+        /// <returns>参数值</returns>
+        //IEnumerable<Param> QueryParam(string name);
+
+        /// <summary>
+        /// 写入参数 
+        /// </summary>
+        /// <param name="para">参数</param>
+        void InsertParam(Param para);
+
+        /// <summary>
+        /// 写入数据或修改数据
+        /// </summary>
+        /// <param name="data">待写入的数据</param>
+        /// <returns></returns>
+        void InsertData<T>(T data) where T : RealTimeData;
+
+        /// <summary>
+        /// 查询数据
+        /// </summary>
+        /// <param name="queryCondition">查询条件</param>
+        /// <returns>返回的数据集合</returns>
+        //IEnumerable<T> QueryData<T>(object queryCondition);
+
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="delCondition">删除条件</param>
+        /// <returns>受影响数据数量</returns>
+        int DeleteData<T>(object delCondition) where T : RealTimeData;
+
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="delCondition">删除条件,必须用where开头的字符串条件</param>
+        /// <returns>受影响数据数量</returns>
+        //int DeleteData<T>(string delConditionWhere);
+    }
+
+    public class Param
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class RealTimeData
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? SiteNumber { get; set; }
+        public int? SensorNumber { get; set; }
+        public float? OriginValue { get; set; }
+        public float? CalcValue { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public DateTime? UpdatedTime { get; set; }
+    }
+}
---
+++ DB/MySQLHelper.cs
@@ -0,0 +1,344 @@
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System;
+
+namespace DataBase
+{
+    public class MySQLHelper : IDataBase
+    {
+        MySqlConnection mysqlConnection;
+        bool isStatistics;
+        /// <summary>
+        /// 配置数据库连接参数
+        /// </summary>
+        /// <param name="server">数据库服务器地址</param>
+        /// <param name="port">数据库服务器端口</param>
+        /// <param name="user">用户名</param>
+        /// <param name="password">密码</param>
+        /// <param name="isStatistics">是否需要统计实时数据</param>
+        public MySQLHelper(string server, string port, string user, string password, bool isStatistics = true)
+        {
+            string connStr = "datasource=" + server
+                + ";port=" + port
+                + ";user=" + user
+                + ";pwd=" + password + ";";
+            mysqlConnection = new MySqlConnection(connStr);
+            SimpleCRUD.SetDialect(SimpleCRUD.Dialect.MySQL);
+            this.isStatistics = isStatistics;
+        }
+        /// <summary>
+        /// 连接数据库
+        /// </summary>
+        /// <returns>连接是否成功</returns>
+        public bool Connect()
+        {
+            try
+            {
+                mysqlConnection.Open();
+                string cmdStr = "SELECT * FROM sys.schema_table_statistics where TABLE_SCHEMA='sxddck_db'; ";
+                System.Data.IDataReader result = mysqlConnection.ExecuteReader(cmdStr);
+                bool isExist = result.Read();
+                result.Close();
+                if (!isExist)
+                {
+                    //创建数据库
+                    mysqlConnection.Execute(@"CREATE DATABASE `sxddck_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;");
+                    mysqlConnection.ChangeDatabase("sxddck_db");
+                    //创建6张表
+                    string[] tableNames = isStatistics ? new string[] { "", "_month", "_day", "_hour", "_min" } : new string[] { "" };
+                    foreach (var name in tableNames)
+                    {
+                        CreateTable(name);
+                    }
+                    //创建参数表
+                    CreateParaTable();
+                    if (isStatistics)
+                    {
+                        //创建统计触发器
+                        CreateTriggers();
+                    }
+                }
+                else
+                {
+                    mysqlConnection.ChangeDatabase("sxddck_db");
+                }
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+        private void CreateTriggers()
+        {
+            mysqlConnection.Execute(@"
+                DROP TRIGGER IF EXISTS `tri_realTimeMinDataInsert`;
+                create trigger tri_realTimeMinDataInsert after insert
+                on realtimedata for each row
+                begin
+                DECLARE curr DATETIME;
+                DECLARE curr_min DATETIME;
+                DECLARE last_min_sum DOUBLE;
+                DECLARE is_exists_min INT DEFAULT 0;
+
+                SET curr = new.CreatedTime;
+                set curr_min = SUBDATE(curr,INTERVAL SECOND(curr) SECOND);
+                set last_min_sum = convert((select max(OriginValue) from realtimedata_min where SiteNumber =new.SiteNumber and SensorNumber = new.SensorNumber and CreatedTime = curr_min),decimal(10,3));
+
+                SELECT COUNT(*) INTO is_exists_min FROM realtimedata_min WHERE SiteNumber = new.SiteNumber AND 
+                SensorNumber=new.SensorNumber AND CreatedTime = curr_min;
+
+                IF is_exists_min>0 THEN
+                UPDATE realtimedata_min SET UpdatedTime = curr, `Count` = `Count`+1,OriginValue = 
+                new.OriginValue +last_min_sum, CalcValue = (new.OriginValue+last_min_sum)/`Count`
+                WHERE SiteNumber = new.SiteNumber AND SensorNumber=new.SensorNumber AND CreatedTime = curr_min;
+                ELSE 
+                INSERT INTO realtimedata_min
+                (SiteNumber,SensorNumber,OriginValue,CalcValue,`Count`,CreatedTime,UpdatedTime)
+                VALUES(new.SiteNumber,new.SensorNumber,new.OriginValue,new.OriginValue,1,curr_min,curr);
+                END IF ;
+
+                end");
+            mysqlConnection.Execute(@"
+                DROP TRIGGER IF EXISTS `tri_realTimeHourDataInsert`; 
+                create trigger tri_realTimeHourDataInsert after insert
+                on realtimedata for each row
+                begin
+                DECLARE curr DATETIME;
+                DECLARE curr_min DATETIME;
+                DECLARE curr_hour DATETIME;
+                DECLARE last_hour_sum DOUBLE;
+                DECLARE is_exists_hour INT DEFAULT 0;
+
+                SET curr = new.CreatedTime;
+                set curr_min = SUBDATE(curr,INTERVAL SECOND(curr) SECOND);
+                set curr_hour = SUBDATE(curr_min,INTERVAL MINUTE(curr_min) MINUTE);
+                set last_hour_sum = convert((select max(OriginValue) from realtimedata_hour where SiteNumber =new.SiteNumber and SensorNumber = new.SensorNumber and CreatedTime = curr_hour),decimal(10,3));
+
+                SELECT COUNT(*) INTO is_exists_hour FROM realtimedata_hour WHERE SiteNumber = new.SiteNumber AND 
+                SensorNumber=new.SensorNumber AND CreatedTime = curr_hour;
+
+                IF is_exists_hour>0 THEN
+                UPDATE realtimedata_hour SET UpdatedTime = curr, `Count` = `Count`+1,OriginValue = 
+                new.OriginValue +last_hour_sum, CalcValue = (new.OriginValue+last_hour_sum)/`Count`
+                WHERE SiteNumber = new.SiteNumber AND SensorNumber=new.SensorNumber AND CreatedTime = curr_hour;
+                ELSE 
+                INSERT INTO realtimedata_hour
+                (SiteNumber,SensorNumber,OriginValue,CalcValue,`Count`,CreatedTime,UpdatedTime)
+                VALUES(new.SiteNumber,new.SensorNumber,new.OriginValue,new.OriginValue,1,curr_hour,curr);
+                END IF ;
+
+                end
+                ");
+            mysqlConnection.Execute(@"
+                DROP TRIGGER IF EXISTS `tri_realTimeDayDataInsert`; 
+                create trigger tri_realTimeDayDataInsert after insert
+                on realtimedata for each row
+                begin
+                DECLARE curr DATETIME;
+                DECLARE curr_min DATETIME;
+                DECLARE curr_hour DATETIME;
+                DECLARE curr_day DATETIME;
+                DECLARE last_day_sum DOUBLE;
+                DECLARE is_exists_day INT DEFAULT 0;
+
+                SET curr = new.CreatedTime;
+                set curr_min = SUBDATE(curr,INTERVAL SECOND(curr) SECOND);
+                set curr_hour = SUBDATE(curr_min,INTERVAL MINUTE(curr_min) MINUTE);
+                set curr_day = SUBDATE(curr_hour,INTERVAL HOUR(curr_hour) HOUR);
+                set last_day_sum = convert((select max(OriginValue) from realtimedata_day where SiteNumber =new.SiteNumber and SensorNumber = new.SensorNumber and CreatedTime = curr_day),decimal(10,3));
+
+                SELECT COUNT(*) INTO is_exists_day FROM realtimedata_day WHERE SiteNumber = new.SiteNumber AND 
+                SensorNumber=new.SensorNumber AND CreatedTime = curr_day;
+
+                IF is_exists_day>0 THEN
+                UPDATE realtimedata_day SET UpdatedTime = curr, `Count` = `Count`+1,OriginValue = 
+                new.OriginValue +last_day_sum, CalcValue = (new.OriginValue+last_day_sum)/`Count`
+                WHERE SiteNumber = new.SiteNumber AND SensorNumber=new.SensorNumber AND CreatedTime = curr_day;
+                ELSE 
+                INSERT INTO realtimedata_day
+                (SiteNumber,SensorNumber,OriginValue,CalcValue,`Count`,CreatedTime,UpdatedTime)
+                VALUES(new.SiteNumber,new.SensorNumber,new.OriginValue,new.OriginValue,1,curr_day,curr);
+                END IF ;
+
+                end");
+            mysqlConnection.Execute(@"
+                DROP TRIGGER IF EXISTS `tri_realTimeMonthDataInsert`; 
+                create trigger tri_realTimeMonthDataInsert after insert
+                on realtimedata for each row
+                begin
+                DECLARE curr DATETIME;
+                DECLARE curr_min DATETIME;
+                DECLARE curr_hour DATETIME;
+                DECLARE curr_day DATETIME;
+                DECLARE curr_month DATETIME;
+                DECLARE last_month_sum DOUBLE;
+                DECLARE is_exists_month INT DEFAULT 0;
+
+                SET curr = new.CreatedTime;
+                set curr_min = SUBDATE(curr,INTERVAL SECOND(curr) SECOND);
+                set curr_hour = SUBDATE(curr_min,INTERVAL MINUTE(curr_min) MINUTE);
+                set curr_day = SUBDATE(curr_hour,INTERVAL HOUR(curr_hour) HOUR);
+                set curr_month = SUBDATE(curr_day,INTERVAL DAY(curr_day)-1 DAY);
+                set last_month_sum = convert((select max(OriginValue) from realtimedata_month where SiteNumber =new.SiteNumber and SensorNumber = new.SensorNumber and CreatedTime = curr_month),decimal(10,3));
+
+                SELECT COUNT(*) INTO is_exists_month FROM realtimedata_month WHERE SiteNumber = new.SiteNumber AND 
+                SensorNumber=new.SensorNumber AND CreatedTime = curr_month;
+
+                IF is_exists_month>0 THEN
+                UPDATE realtimedata_month SET UpdatedTime = curr, `Count` = `Count`+1,OriginValue = 
+                new.OriginValue +last_month_sum, CalcValue = (new.OriginValue+last_month_sum)/`Count`
+                WHERE SiteNumber = new.SiteNumber AND SensorNumber=new.SensorNumber AND CreatedTime = curr_month;
+                ELSE 
+                INSERT INTO realtimedata_month
+                (SiteNumber,SensorNumber,OriginValue,CalcValue,`Count`,CreatedTime,UpdatedTime)
+                VALUES(new.SiteNumber,new.SensorNumber,new.OriginValue,new.OriginValue,1,curr_month,curr);
+                END IF ;
+
+                end
+            ");
+        }
+        /// <summary>
+        /// 断开连接
+        /// </summary>
+        public void Disconnect()
+        {
+            mysqlConnection.Close();
+        }
+        private void CreateParaTable()
+        {
+            mysqlConnection.Execute(
+                @"CREATE TABLE `param` (
+                `Id` int(11) NOT NULL AUTO_INCREMENT,
+                `Name` varchar(45) DEFAULT NULL,
+                `Value` varchar(45) DEFAULT NULL,
+                PRIMARY KEY (`Id`)
+                ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
+            );
+        }
+        private void CreateTable(string tableName)
+        {
+            //创建表
+            mysqlConnection.Execute(
+                @"CREATE TABLE `realtimedata" + tableName + @"` (
+                `Id` int(11) NOT NULL AUTO_INCREMENT,
+                `SiteNumber` int(11) DEFAULT NULL,
+                `SensorNumber` int(11) DEFAULT NULL,
+                `OriginValue` float DEFAULT NULL,
+                `CalcValue` float DEFAULT NULL,
+                `Count` int(11) DEFAULT NULL,
+                `CreatedTime` datetime DEFAULT NULL,
+                `UpdatedTime` datetime DEFAULT NULL,
+                PRIMARY KEY(`Id`)
+                ) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci"
+            );
+        }
+        /// <summary>
+        /// 存入参数
+        /// </summary>
+        /// <param name="param">参数值</param>
+        public void InsertParam(Param param)
+        {
+            mysqlConnection.Insert(param);
+        }
+        /// <summary>
+        /// 查询参数
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>返回参数集合</returns>
+        public IEnumerable<Param> QueryParam(string name)
+        {
+            return mysqlConnection.GetList<Param>(new { Name = name });
+        }
+        /// <summary>
+        /// 依据参数名删除参数表
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public int DeleteParam(string name)
+        {
+            return mysqlConnection.DeleteList<Param>(new { Name=name });
+        }
+        /// <summary>
+        /// 删除所有参数
+        /// </summary>
+        /// <returns></returns>
+        public int DeleteAllParams()
+        {
+            return mysqlConnection.DeleteList<Param>("where Id <> -1");
+        }
+        /// <summary>
+        /// 插入数据
+        /// </summary>
+        /// <typeparam name="T">数据库映射类</typeparam>
+        /// <param name="data">数据</param>
+        public void InsertData<T>(T data) where T:RealTimeData
+        {
+            mysqlConnection.Insert(data);
+        }
+        /// <summary>
+        /// 查询数据,该接口暂时不开放
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        private IEnumerable<T> QueryData<T>(object condition = null) where T : RealTimeData
+        {
+            if (condition != null)
+            {
+                var result = mysqlConnection.GetList<T>(condition);
+                return result;
+            }
+            else
+            {
+                var result = mysqlConnection.GetList<T>();
+                return result;
+            }
+        }
+        /// <summary>
+        /// 特殊数据查询接口
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="condition"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        internal IEnumerable<T> QueryData<T>(string condition, object param = null) where T : RealTimeData
+        {
+            var result = mysqlConnection.GetList<T>(condition, param);
+            return result;
+        }
+        /// <summary>
+        /// 用开始和结束时间进行查询的接口
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="startTime">查询起始时间</param>
+        /// <param name="endTime">查询结束时间</param>
+        /// <returns>返回可枚举的记录集合</returns>
+        public IEnumerable<T> QueryData<T>(DateTime startTime, DateTime endTime) where T : RealTimeData
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("startTime", startTime);
+            dynamicParameters.Add("endTime", endTime);
+            return QueryData<T>(@"where CreatedTime >= @startTime and CreatedTime <= @endTime",dynamicParameters);
+        }
+        public int DeleteData<T>(object delCondition = null) where T : RealTimeData
+        {
+            return mysqlConnection.DeleteList<T>(delCondition);
+        }
+        private int DeleteData<T>(string delCondition) where T : RealTimeData
+        {
+            return mysqlConnection.DeleteList<T>(delCondition);
+        }
+    }
+    public class realtimedata : RealTimeData
+    { }
+    public class realtimedata_min : RealTimeData
+    { }
+    public class realtimedata_hour : RealTimeData
+    { }
+    public class realtimedata_day : RealTimeData
+    { }
+    public class realtimedata_month : RealTimeData
+    { }
+}
